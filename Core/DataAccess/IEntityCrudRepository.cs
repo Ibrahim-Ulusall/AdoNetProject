84896df4d8943c92176baf372f6a8e96f12cf15c
ibrahim <ibrahimulusal185@gmail.com>
2023-06-30 20:26:21 +0300

    Results Added

---
+++ Core/DataAccess/IEntityCrudRepository.cs
@@ -0,0 +1,14 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DataAccess
+{
+	public interface IEntityCrudRepository<TEntity> where TEntity: class , IEntity, new()
+	{
+
+	}
+}
---
+++ Core/Utilities/Results/SuccessResult.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Utilities.Results
+{
+	public class SuccessResult:Result
+	{
+        public SuccessResult(string message,bool success):base(message,true)
+        {
+            
+        }
+        public SuccessResult(string message): base(message,true)
+        {
+            
+        }
+        public SuccessResult():base(true)
+        {
+
+        }
+    }
+}
